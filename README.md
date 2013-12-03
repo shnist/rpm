97c00167f9385cc8dcc63f4a752a24e8e1edb3e5
shnist <aaron.jack.faber@gmail.com>
2013-12-03 18:20:22 +0000

    Updated gitignore and basic playlist functionality included

--- README.md
+++ README.md
@@ -3,12 +3,20 @@ rpm
 
 Physicalising music
 
-
 ## Dependencies
 
-### NPM install
+### NPM
 npm install -g node-gyp
 
-### Clone the lib-spotify library
-git clone https://github.com/FrontierPsychiatrist/node-spotify.git lib-spotify
+### Lib spotify
+If you use `brew` then you can install lib-spotify pretty easily using:
 
+`brew install libspotify`
+
+## Compiling the lib-spotify library
+* Clone the library onto your machine
+`git clone https://github.com/FrontierPsychiatrist/node-spotify.git lib-spotify`
+* CD into the root of the project
+* Run `node-gyp configure && node-gyp build`
+* This will compile the library to use as a node module. This is located in `/build/Release/`
+* Copy the contents of this folder into a folder called `spotify` into the RPM project (at root level)
--- app.js
+++ app.js
@@ -1,7 +1,41 @@
-var spotify = require('spotify')({ appkeyFile: 'spotify_appkeyFile.key' });
-spotify.ready( function() {
- var starredTracks = spotify.getStarred().getTracks();
- console.log(starredTracks);
- spotify.player.play(starredTracks[0]);
+var spotify = require('./spotify/spotify')( {
+	appkeyFile: './spotify_appkey.key'
 });
-spotify.login('aaronfaber', '23!Arsakia1089', false, false);
+
+spotify.ready(function() {
+	var playlists;
+	playlists = spotify.getPlaylists();
+
+
+	var filteredPlaylists = [];
+
+	for (var i = 0; i < playlists.length; i++) {
+		if (playlists[i].name.length > 0) {
+			filteredPlaylists.push(playlists[i]);
+		}
+	}
+
+	//console.log(filteredPlaylists[0]);
+
+
+	var search = new spotify.Search('Lady Gaga – A Very Gaga Holiday');
+	search.execute(function (error, searchResult) {
+		console.log(searchResult);
+		var playlistResult = searchResult.playlists[0];
+		var track = playlistResult.getTracks()[0];
+
+		spotify.player.play(track);
+	});
+
+/*	playlist.on('playlist_renamed', function(err, playlist) {
+		console.log(playlist.name);
+	});*/
+
+/*	playlists.on('playlist_tracks_added', function(err, tracks) {
+		console.log(tracks);
+	});*/
+	//console.log(playlists)
+});
+
+
+spotify.login('aaronfaber', '23!Arsakia1089', true, false);
