d76de85da5bc7eddb4c6aa52339881c37b76287c
swansonb <bjsswanson@gmail.com>
2013-12-04 09:54:18 +0000

    Making ImageMatcher self contained.

--- image_matcher/src/main/java/com/swansonb/imagematching/ImageMatcherApplication.java
+++ image_matcher/src/main/java/com/swansonb/imagematching/ImageMatcherApplication.java
@@ -1,26 +1,24 @@
 package com.swansonb.imagematching;
 
+import com.swansonb.imagematching.utils.NativeUtils;
 import org.opencv.core.Core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 
 @ComponentScan
 @EnableAutoConfiguration
 public class ImageMatcherApplication {
 
-	public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+	public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, IOException {
 		loadLibrary();
 		SpringApplication.run(ImageMatcherApplication.class, args);
 	}
 
-	private static void loadLibrary() throws NoSuchFieldException, IllegalAccessException {
-		System.setProperty("java.library.path", "native");
-		Field fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
-		fieldSysPath.setAccessible(true);
-		fieldSysPath.set(null, null);
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	private static void loadLibrary() throws NoSuchFieldException, IllegalAccessException, IOException {
+		NativeUtils.loadLibraryFromJar("/native/libopencv_java247.dylib");
 	}
 }
