9d57a16f2c5cd6413db67d76e8e99cb1f83fb57c
shnist <aaron.jack.faber@gmail.com>
2014-05-17 19:24:26 +0100

    Renamed spotify wrapper to spotify interface

---
+++ spotify/spotify_interface.js
@@ -0,0 +1,28 @@
+var spotifyOptions = {
+	appkeyFile: 'spotify_appkey.key'
+};
+
+var spotify = require('node-spotify')(spotifyOptions);
+
+spotify.login('aaron.jack.faber@gmail.com', '23Arsakia1089', false, false);
+
+spotify.ready(function () {
+	searchSpotify();
+});
+
+
+function searchSpotify () {
+	var search = new spotify.Search('Lady Gaga – A Very Gaga Holiday');
+	search.execute(function (error, searchResult) {
+	    //console.log(searchResult);
+	    var playlistResult = searchResult.playlists[0];
+	    var track = playlistResult.getTracks()[0];
+
+	    spotify.player.play(track);
+	    spotify.player.seek(track.duration - 4);
+
+	    spotify.player.on('player_end_of_track', function (error, player) {
+	    	
+	    });
+	});
+}
