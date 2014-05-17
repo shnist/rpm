b21218baa4fc7625016f1ca5fcf38ffa6ec9d770
shnist <aaron.jack.faber@gmail.com>
2014-05-17 19:36:43 +0100

    Added the skeleton of tests

---
+++ tests/spotify_interface_test.js
@@ -0,0 +1,31 @@
+var sinon = require('sinon'),
+	chai = require('chai');
+
+describe('spotify/spotify_interface', function () {
+
+	describe('Given I want to login', function () {
+
+		describe('When I attempt to log in with VALID user credentials', function () {
+
+			it('should return with a status of 200', function () {
+
+			});
+
+			it('should return with a successful status message', function () {
+
+			});
+		});
+
+
+		describe('When I attempt to log in with INVALID user credentials', function () {
+
+			it('should return with a status of 401', function () {
+
+			});
+
+			it('should return with an unauthorised status message', function () {
+
+			});
+		});
+	});	
+});
