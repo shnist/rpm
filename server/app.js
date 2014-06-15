7c8c00ddc013d17a5bd46f9bc9a19d40fe9db571
shnist <aaron.jack.faber@gmail.com>
2014-06-15 14:36:22 +0100

    Converted to hapi.js for node server

--- server/app.js
+++ server/app.js
@@ -1,28 +1,51 @@
 'use strict';
 
-var express = require('express'),
-    http = require('http'),
-    app = module.exports = express(),
-    port = 3010,
-    appPath = __dirname + '/../app',
-    server;
+var Hapi = require('hapi'),
+    server = new Hapi.Server('localhost', 9000);
 
-app.configure(function() {
-    app.use(express.favicon());
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.cookieParser('SIDD1H6WBHP7HME2FXY9M7HM06L4PDIIIRBF9417'));
-    app.use(express.session({
-        path: '*',
-        httpOnly: true,
-        maxAge: 60000 * 60 * 24 * 30
-    }));
-    app.use(express.static(appPath));
+server.start(function () {
+    console.log('Started server at ' + server.info.host + ':' + server.info.port);
 });
 
-require('./routes/spotify')(app);
+server.route({
+    method: 'post',
+    path: '/v1/login',
+    handler: function (request, reply) {
+        reply('login');
+    }
+});
 
-server = http.createServer(app);
-server.listen(port, '0.0.0.0');
+server.route({
+    method: 'get',
+    path: '/v1/search',
+    handler: function (request, reply) {
+        reply('search');
+    }
+});
 
-console.log('Listening to port ', port);
+server.route({
+    method: 'get',
+    path: '/v1/tracks/{id}',
+    handler: function (request, reply) {
+        console.log(request.params.id);
+        reply('tracks');
+    }
+});
+
+server.route({
+    method: 'get',
+    path: '/v1/playlists/{id}',
+    handler: function (request, reply) {
+        console.log(request.params.id);
+        reply('playlists');
+    }
+});
+
+server.route({
+    method: 'get',
+    path: '/v1/albums/{id}',
+    handler: function (request, reply) {
+        console.log(request.params.id);
+        reply('albums');
+    }
+});
